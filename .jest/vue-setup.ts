bb356394667eae1b48655fdd36efa73b64169768
Morco <morco.mail@gmail.com>
2020-04-20 13:54:50 +0300

    Fixed jest imports

--- .jest/vue-setup.ts
+++ .jest/vue-setup.ts
@@ -1,16 +1,14 @@
-import * as Vue from 'vue';
-import * as Quasar from 'quasar';
-import * as VueI18n from 'vue-i18n';
+import Vue from 'vue';
+import Quasar from 'quasar';
+import VueI18n from 'vue-i18n';
 import enUS from '../src/i18n/en-us';
 
 
-// @ts-ignore
 Vue.use(VueI18n);
 // @ts-ignore
 Vue.use(Quasar);
 
 // create global mixin
-// @ts-ignore
 Vue.mixin({
    computed: {
       YLocale(): any {
@@ -20,7 +18,6 @@ Vue.mixin({
    },
 });
 
-// @ts-ignore
 export const i18n = new VueI18n({
    locale: 'en-us',
    fallbackLocale: 'en-us',
